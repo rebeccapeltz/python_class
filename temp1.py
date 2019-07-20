cea37674731b47cdf6e129dbd39d8b2de9a72861
rebeccapeltz <rebeccapeltz@gmail.com>
2019-07-20 11:19:40 -0700

    first commit

---
+++ temp1.py
@@ -0,0 +1,45 @@
+
+OUT_OF_BOUNDS = float('-inf')
+# print(OUT_OF_BOUNDS)
+
+def calculate(first_number, second_number, operator):
+  result = 0
+  if operator == "+":
+    result = first_number + second_number
+  elif operator == "-":
+    result =first_number - second_number
+  elif operator == "*":
+    result = first_number * second_number
+  elif operator == "/":
+    result = first_number / second_number   
+  else:
+    result = OUT_OF_BOUNDS
+  return result
+
+# print(calculate(1,2,'+'))
+
+# set a boolean to true to continue asking for input
+more_input = True
+
+while more_input:
+
+  first_number = input("What is the first number? ")
+  first_number = int(first_number)
+
+  second_number = input("What is the second number? ")
+  second_number = int(second_number)
+
+  operator = input("What operation do you want me to do (+, -, *, /)? ")
+  operator = operator.strip()
+
+  result = calculate(first_number, second_number, operator)
+
+  if result == OUT_OF_BOUNDS:
+    print("you need a valid operator (+, -, *, /)")
+  else:
+    print (result)
+  
+  more_input = bool(True if input("Another calculation (y/n)? ")== 'y' else False)
+  
+print ("Bye now")
+
